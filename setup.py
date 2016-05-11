5acbb24eb89df4eee1e49042e1de4d5ef2e9385b
Jason Kang <Min.Liang.Kang@jpl.nasa.gov>
2016-05-11 09:20:00 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,46 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md') as file:
+    long_description = "Update README.md file on a github repository"
+
+setup(
+    name='update-github-readme',
+    version='0.0.1',
+    url='https://github.com/jasonmlkang/update-github-readme',
+    description='Update README.md file on a github repository',
+    author='Jason Kang',
+    license='MIT',
+    py_modules=['update_github_readme'],
+    long_description=long_description,
+    install_requires=[],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    entry_points = """
+[console_scripts]
+pushReadme = update_github_readme:run
+flaskswagger = build_swagger_spec:run
+    """,
+    options={
+        'bdist_rpm':{
+            'build_requires':[
+                'python',
+                'python-setuptools',
+                ],
+            'requires':[
+                'python',
+                'python-setuptools',
+            ],
+        },
+    },
+)
